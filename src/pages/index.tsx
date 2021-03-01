ef92100b29ad2b3437e5adda40870e30ede25d52
CostaPedroHenrique <devpedrohenrique@gmail.com>
2021-03-01 19:19:51 -0300

    Base do projeto com next js

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/pages/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Open Currículo Vitae</title>
+      </Head>
+      <h2>OPEN CV!</h2>
+      <section>
+        <div></div>
+      </section>
+    </div>
+  );
+}
